99063853cc55e532c44e120dd19f515b1b68e2e4
Joshua Laurence <laurence11@fb.com>
2022-11-18 16:07:46 +0000

    Prepared project for hosting on render

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const bookRouter = require("../routes/book");
 const app = express()
 const db = require("../db/db")
 const seed = require("../db/seed")
-
+const port = process.env.PORT || 5002
 const cors = require("cors")
 
 app.use(cors())
@@ -13,6 +13,6 @@ app.use("/books", bookRouter)
 
 seed();
 
-app.listen(5002, () => {
-    console.log("Backend listening on port 5002")
+app.listen(port, () => {
+    console.log(`Backend listening on port ${port}`)
 })
