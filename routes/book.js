e38c0a7f0db1cf1c69b4271f5661d4e2db5c357d
Joshua Laurence <joshua@Joshuas-MacBook-Air.local>
2023-01-03 07:24:09 +0000

    Added deletion endpoint

--- routes/book.js
+++ routes/book.js
@@ -1,53 +1,65 @@
-const express = require("express");
-const generateCoOrds = require("../db/coords");
-const db = require("../db/db");
-const {Book} = require("../models")
+const express = require('express');
+const generateCoOrds = require('../db/coords');
+const db = require('../db/db');
+const seed = require('../db/seed');
+const { Book } = require('../models');
 
 const bookRouter = express.Router();
 
-bookRouter.get("/sync", async (request, response) => {
-    //await db.sync({force: true})
-    response.status(200).send("Database Synced")
-})
+bookRouter.get('/seed', async (request, response) => {
+	seed();
+	response.status(200).send('Database seeded');
+});
 
-bookRouter.get("/", async (request, response) => {
-    try {
-        const allBooks = await Book.find({});
-        if (allBooks === []) {
-            throw new Error("The library is empty...")
-        }
-        response.status(200).send(allBooks)
-    } catch (error) {
-        response.status(400).send(error.message)
-    }
-})
+bookRouter.get('/sync', async (request, response) => {
+	//await db.sync({force: true})
+	response.status(200).send('Database Synced');
+});
 
-bookRouter.get("/:bookID", async (request, response) => {
-    try {
-        const chosenBook = await Book.findById(request.params.bookID);
-        if (chosenBook === null) {
-            throw new Error("This book doesn't exist")
-        }
-        response.status(200).send(chosenBook)
-    } catch (error) {
-        response.status(404).send(error.message)
-    }
-})
+bookRouter.get('/', async (request, response) => {
+	try {
+		const allBooks = await Book.find({});
+		if (allBooks === []) {
+			throw new Error('The library is empty...');
+		}
+		response.status(200).send(allBooks);
+	} catch (error) {
+		response.status(400).send(error.message);
+	}
+});
 
-bookRouter.post("/", async (request, response) => {
-    try {
-        console.log(request.body)
-        const results = await generateCoOrds()
-        const pos = {position: results[0]}
-        const rot = {rotation: results[1]}
-        const data = {...request.body, ...pos, ...rot}
-        console.log(data)
-        const chosenBook = await Book.create(data);
-        response.status(200).send(chosenBook)
-    } catch (error) {
-        response.status(500).send(error.message)
-    }
-})
+bookRouter.get('/:bookID', async (request, response) => {
+	try {
+		const chosenBook = await Book.findById(request.params.bookID);
+		if (chosenBook === null) {
+			throw new Error("This book doesn't exist");
+		}
+		response.status(200).send(chosenBook);
+	} catch (error) {
+		response.status(404).send(error.message);
+	}
+});
 
+bookRouter.post('/', async (request, response) => {
+	try {
+		const results = await generateCoOrds();
+		const pos = { position: results[0] };
+		const rot = { rotation: results[1] };
+		const data = { ...request.body, ...pos, ...rot };
+		const chosenBook = await Book.create(data);
+		response.status(200).send(chosenBook);
+	} catch (error) {
+		response.status(500).send(error.message);
+	}
+});
+
+bookRouter.post('/delete/:bookID', async (request, response) => {
+	try {
+		const results = await Book.findByIdAndDelete(request.params.bookID);
+		response.status(200).send('Book successfully deleted');
+	} catch (error) {
+		response.status(500).send(error.message);
+	}
+});
 
 module.exports = bookRouter;
--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
-require("../db/db")
-const express = require("express");
-const bookRouter = require("../routes/book");
-const app = express()
-const db = require("../db/db")
-const seed = require("../db/seed")
-const port = process.env.PORT || 5002
-const cors = require("cors")
+// require("../db/db")
+const express = require('express');
+const bookRouter = require('../routes/book');
+const app = express();
+const db = require('../db/db');
+const port = process.env.PORT || 5002;
+const cors = require('cors');
 
-app.use(cors())
-app.use(express.json())
-app.use("/books", bookRouter)
+app.use(cors());
+app.use(express.json());
+app.use('/books', bookRouter);
 
-seed();
+// seed();
 
 app.listen(port, () => {
-    console.log(`Backend listening on port ${port}`)
-})
+	console.log(`Backend listening on port ${port}`);
+});
